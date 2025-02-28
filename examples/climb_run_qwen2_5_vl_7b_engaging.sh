85425cdc92df350d2719e30efed6a99fdaab859f
DVD <zjdavid.2003@gmail.com>
2025-02-28 16:11:48 -0500

    Add 7B training script

---
+++ examples/climb_run_qwen2_5_vl_7b_engaging.sh
@@ -0,0 +1,15 @@
+set -x
+
+/home/dvdai/miniconda3/bin/conda activate test
+
+export VLLM_ATTENTION_BACKEND=XFORMERS
+#export MKL_SERVICE_FORCE_INTEL=1
+
+python -m verl.trainer.main \
+    config=examples/grpo_climb_engaging.yaml \
+    data.max_prompt_length=4096 \
+    worker.actor.model.model_path=Qwen/Qwen2.5-VL-7B-Instruct \
+    worker.rollout.tensor_parallel_size=1 \
+    worker.rollout.enable_chunked_prefill=false \
+    trainer.experiment_name=qwen2_5_vl_7b_climb \
+    trainer.n_gpus_per_node=4
