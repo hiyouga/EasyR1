ba5f06c9934cbbcb232fbde039fe43d1b1416d5c
Ziyi <1034337098@qq.com>
2025-10-15 09:56:35 +0000

    .

--- examples/glm4.1v_base_geo3k_grpo.sh
+++ examples/glm4.1v_base_geo3k_grpo.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-MODEL_PATH=zai-org/GLM-4.1V-9B-Thinking  # replace it with your local file path
+MODEL_PATH=zai-org/GLM-4.1V-9B-Base  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.train_files=hiyouga/geometry3k@train \
     data.val_files=hiyouga/geometry3k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.experiment_name=glm4.1v_thinking_geo_grpo \
+    trainer.experiment_name=glm4.1v_base_geo_grpo \
     worker.actor.padding_free=False \
     trainer.n_gpus_per_node=8
